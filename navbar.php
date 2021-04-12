179dbd0fd97f5d39ce5d929d054e3b0aabf7d9f6
AnjaliAnil25 <82399933+AnjaliAnil25@users.noreply.github.com>
2021-04-12 21:48:11 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,26 @@
+<section id="nav-bar">
+    <nav class="navbar navbar-expand-lg navbar-light ">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index1.php">Home</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ">
+            <!-- to move compts towards right-->
+            <li class="nav-item">
+              <a class="nav-link" href="createuser.php">Create User</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="transfer.php">Tranfer</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="transhistory.php">Transaction History</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </section>
+  
